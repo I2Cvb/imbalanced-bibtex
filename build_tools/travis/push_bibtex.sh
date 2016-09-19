012db10d3a89a5e2e4f54de2f6011d2c7464bbce
Guillaume Lemaitre <glemaitre@visor.udg.edu>
2016-09-19 19:39:34 +0200

    Trigger pushing only on master push

--- build_tools/travis/push_bibtex.sh
+++ build_tools/travis/push_bibtex.sh
@@ -1,19 +1,22 @@
 #!/bin/bash
 
-set -o errexit
+# Only make a push if we build the master branch
+if [ "$TRAVIS_BRANCH" == "master" ]; then
+    set -o errexit
 
-cd $TRAVIS_BUILD_DIR
+    cd $TRAVIS_BUILD_DIR
 
-# Order the bibtex
-python sort_bibtex.py
+    # Order the bibtex
+    python sort_bibtex.py
 
-git remote set-url origin "https://glemaitre:${GH_TOKEN}@github.com/I2Cvb/imbalanced-bibtex.git"
+    git remote set-url origin "https://glemaitre:${GH_TOKEN}@github.com/I2Cvb/imbalanced-bibtex.git"
 
-git config --global user.email $EMAIL
-git config --global user.name $USERNAME
-git add imbalanced-bibtex.bib
-git commit -m "Sort the bibtex file"
+    git config --global user.email $EMAIL
+    git config --global user.name $USERNAME
+    git add imbalanced-bibtex.bib
+    git commit -m "Sort the bibtex file"
 
-echo "Done with the commit, let's push"
+    echo "Done with the commit, let's push"
 
-git push origin master
+    git push origin master
+fi
